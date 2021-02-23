48c42aef37c3c9e02f1c117d0cce94003988d865
LaNeitD18 <phanhuytien14@gmail.com>
2021-02-23 20:33:46 +0700

    Setup pageview for home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,77 @@
+import 'package:SkypeClone/utils/universal_variables.dart';
 import 'package:flutter/material.dart';
 import 'package:SkypeClone/resources/firebase_repository.dart';
+import 'package:flutter/cupertino.dart';
 
-class HomeScreen extends StatelessWidget {
-  //const HomeScreen({Key key}) : super(key: key);
-  FirebaseRepository _repository = FirebaseRepository();
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlatButton(
-        onPressed: () => _repository.signOut(),
-        child: Text(
-          "homing",
-          style: TextStyle(fontSize: 50),
+      backgroundColor: UniversalVariables.blackColor,
+      body: PageView(
+        children: <Widget>[
+          Center(child: Text("Chat List Screen")),
+          Center(child: Text("Call Logs")),
+          Center(child: Text("Contact Screen")),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: CupertinoTabBar(
+            backgroundColor: UniversalVariables.blackColor,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.chat,
+                      color: (_page == 0)
+                          ? UniversalVariables.lightBlueColor
+                          : UniversalVariables.greyColor),
+                  label: "Chats"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.call,
+                      color: (_page == 1)
+                          ? UniversalVariables.lightBlueColor
+                          : UniversalVariables.greyColor),
+                  label: "Call"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.contact_phone,
+                      color: (_page == 2)
+                          ? UniversalVariables.lightBlueColor
+                          : UniversalVariables.greyColor),
+                  label: "Contact"),
+            ],
+            onTap: navigationTapped,
+            currentIndex: _page,
+          ),
         ),
       ),
     );
